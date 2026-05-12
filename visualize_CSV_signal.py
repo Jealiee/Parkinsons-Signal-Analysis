ccc386c8e486312d2e51476e983b9555474fc080
Jealie <adamus_julia@wp.pl>
2026-05-12 12:13:33 +0200

    Add checking time mean

--- visualize_CSV_signal.py
+++ visualize_CSV_signal.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from pathlib import Path
 
 data_dir = Path("data/Natalia/CSV") 
 
-df = pd.read_csv(data_dir / "aux1_2.csv", sep=";", header=None, names=["time", "signal"])
+df = pd.read_csv(
+    data_dir / "aux1_2.csv",
+    sep=";",
+    header=None,
+    names=["time", "signal"]
+)
 
 plt.plot(df["time"], df["signal"])
 plt.xlabel("Time")
 plt.ylabel("Signal Values")
 plt.show()
+
+dt = np.diff(df["time"])
+print(np.mean(dt))
